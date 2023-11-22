e7012037fd402037f665d40cc2ad3e1cf96eefb1
Szymon Richert <szymon.rychu@gmail.com>
2023-11-22 20:58:40 +0100

    Fix renovate not updating Chart version

---
+++ .github/bump_chart_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -euo pipefail
+
+readonly CHART_DIR="${1}"
+readonly CHART_YAML_PATH="${CHART_DIR}/Chart.yaml"
+
+echo "Bumping patch version in '${CHART_YAML_PATH}'"
+
+readonly CURRENT_VERSION="$(grep '^version:' "${CHART_YAML_PATH}" | awk '{print $2}')"
+readonly MAJOR="$(echo "${CURRENT_VERSION}" | cut -d. -f1)"
+readonly MINOR="$(echo "${CURRENT_VERSION}" | cut -d. -f2)"
+readonly PATCH="$(echo "${CURRENT_VERSION}" | cut -d. -f3)"
+readonly NEW_PATCH="$(expr "${PATCH}" + 1)"
+
+echo "Upgrading '${MAJOR}.${MINOR}.${PATCH}' to '${MAJOR}.${MINOR}.${NEW_PATCH}'"
+
+sed -i \"s/^version:.*/version: ${MAJOR}.${MINOR}.${NEW_PATCH}/g\" "${CHART_YAML_PATH}"
